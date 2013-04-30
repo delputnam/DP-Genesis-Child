09e4f16fff902e7a9c594fbdcc91bc1f069fdaa8
Del Putnam <del@putnams.net>
2013-04-30 12:56:31 -0400

    Initial commit for custom base child theme

---
+++ config.rb
@@ -0,0 +1,32 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+# Set this to the root of your project when deployed:
+http_path = "/wp-content/themes/" + File.basename(Dir.getwd)
+# http_path = "/" + File.basename(Dir.getwd)
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+line_comments = false
+
+preferred_syntax = :sass
+
+require 'fileutils'
+on_stylesheet_saved do |file|
+	if File.exists?(file) && File.basename(file) == "style.css"
+		puts "Moving: #{file}"
+		FileUtils.mv(file, File.dirname(file) + "/../" + File.basename(file))
+	end
+end
--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 /**
  * Functions
  *
- * @package      BE_Genesis_Child
+ * @package      DP_Genesis_Child
  * @since        1.0.0
- * @link         https://github.com/billerickson/BE-Genesis-Child
- * @author       Bill Erickson <bill@billerickson.net>
- * @copyright    Copyright (c) 2011, Bill Erickson
+ * @link         https://github.com/delputnam/DP-Genesis-Child
+ * @author       Del Putnam <del@delputnam.com>
+ * @copyright    Copyright (c) 2013, Del Putnam
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  *
  */
@@ -15,7 +15,7 @@
  * Theme Setup
  * @since 1.0.0
  *
- * This setup function attaches all of the site-wide functions 
+ * This setup function attaches all of the site-wide functions
  * to the correct hooks and filters. All the functions themselves
  * are defined below this setup function.
  *
@@ -23,20 +23,20 @@
 
 add_action('genesis_setup','child_theme_setup', 15);
 function child_theme_setup() {
-	
+
 	define( 'CHILD_THEME_VERSION', filemtime( get_stylesheet_directory() . '/style.css' ) );
 
-	// ** Backend **	
-	
+	// ** Backend **
+
 	// Image Sizes
-	// add_image_size( 'be_featured', 400, 100, true );
-	
+	// add_image_size( 'dp_featured', 400, 100, true );
+
 	// Structural Wraps
 	add_theme_support( 'genesis-structural-wraps', array( 'header', 'nav', 'subnav', 'inner', 'footer-widgets', 'footer' ) );
-	
+
 	// Menus
 	add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu' ) );
-	
+
 	// Sidebars
 	unregister_sidebar( 'sidebar-alt' );
 	//genesis_register_sidebar( array( 'name' => 'Blog Sidebar', 'id' => 'blog-sidebar' ) );
@@ -46,9 +46,11 @@ function child_theme_setup() {
 	genesis_unregister_layout( 'content-sidebar-sidebar' );
 	genesis_unregister_layout( 'sidebar-sidebar-content' );
 	genesis_unregister_layout( 'sidebar-content-sidebar' );
-	
+	genesis_unregister_layout( 'sidebar-content' );
+	genesis_unregister_layout( 'content-sidebar' );
+
 	// Remove Unused User Settings
-	add_filter( 'user_contactmethods', 'be_contactmethods' );
+	add_filter( 'user_contactmethods', 'dp_contactmethods' );
 	remove_action( 'show_user_profile', 'genesis_user_options_fields' );
 	remove_action( 'edit_user_profile', 'genesis_user_options_fields' );
 	remove_action( 'show_user_profile', 'genesis_user_archive_fields' );
@@ -60,36 +62,55 @@ function child_theme_setup() {
 
 	// Editor Styles
 	add_editor_style( 'editor-style.css' );
-		
+
 	// Setup Theme Settings
 	include_once( CHILD_DIR . '/lib/functions/child-theme-settings.php' );
-	
+
 	// Reposition Genesis Metaboxes
 	remove_action( 'admin_menu', 'genesis_add_inpost_seo_box' );
-	add_action( 'admin_menu', 'be_add_inpost_seo_box' );
+	add_action( 'admin_menu', 'dp_add_inpost_seo_box' );
 	remove_action( 'admin_menu', 'genesis_add_inpost_layout_box' );
-	add_action( 'admin_menu', 'be_add_inpost_layout_box' );
-	
+	add_action( 'admin_menu', 'dp_add_inpost_layout_box' );
+
 	// Remove Genesis Widgets
-	add_action( 'widgets_init', 'be_remove_genesis_widgets', 20 );
-	
+	add_action( 'widgets_init', 'dp_remove_genesis_widgets', 20 );
+
 	// Remove Genesis Theme Settings Metaboxes
-	add_action( 'genesis_theme_settings_metaboxes', 'be_remove_genesis_metaboxes' );
+	add_action( 'genesis_theme_settings_metaboxes', 'dp_remove_genesis_metaboxes' );
 
 	// Don't update theme
-	add_filter( 'http_request_args', 'be_dont_update_theme', 5, 2 );
-		
-	// ** Frontend **		
-	
+	add_filter( 'http_request_args', 'dp_dont_update_theme', 5, 2 );
+
+	// ** Frontend **
+
+	// Register Stylesheets
+	//wp_register_style( 'dp-style', CHILD_URL . '/css/dp-style.css' );
+
+	// Register Scripts
+	wp_register_script( 'content-script', CHILD_URL . '/js/content-functions.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-affix', CHILD_URL . '/js/bootstrap-affix.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-alert', CHILD_URL . '/js/bootstrap-alert.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-button', CHILD_URL . '/js/bootstrap-button.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-carousel', CHILD_URL . '/js/bootstrap-carousel.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-collapse', CHILD_URL . '/js/bootstrap-collapse.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-dropdown', CHILD_URL . '/js/bootstrap-dropdown.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-modal', CHILD_URL . '/js/bootstrap-modal.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-popover', CHILD_URL . '/js/bootstrap-popover.js', array( 'jquery', 'bootstrap-tooltip' ), false, true );
+	wp_register_script( 'bootstrap-scrollspy', CHILD_URL . '/js/bootstrap-scrollspy.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-tab', CHILD_URL . '/js/bootstrap-tab.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-tooltip', CHILD_URL . '/js/bootstrap-tooltip.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-transition', CHILD_URL . '/js/bootstrap-transition.js', array( 'jquery' ), false, true );
+	wp_register_script( 'bootstrap-typeahead', CHILD_URL . '/js/bootstrap-typeahead.js', array( 'jquery' ), false, true );
+
 	// Remove Edit link
 	add_filter( 'genesis_edit_post_link', '__return_false' );
-	
+
 	// Responsive Meta Tag
-	add_action( 'genesis_meta', 'be_viewport_meta_tag' );
-	
+	add_action( 'genesis_meta', 'dp_viewport_meta_tag' );
+
 	// Footer
 	remove_action( 'genesis_footer', 'genesis_do_footer' );
-	add_action( 'genesis_footer', 'be_footer' );
+	add_action( 'genesis_footer', 'dp_footer' );
 }
 
 // ** Backend Functions ** //
@@ -98,17 +119,17 @@ function child_theme_setup() {
  * Customize Contact Methods
  * @since 1.0.0
  *
- * @author Bill Erickson
+ * @author Del Putnam
  * @link http://sillybean.net/2010/01/creating-a-user-directory-part-1-changing-user-contact-fields/
  *
  * @param array $contactmethods
  * @return array
  */
-function be_contactmethods( $contactmethods ) {
+function dp_contactmethods( $contactmethods ) {
 	unset( $contactmethods['aim'] );
 	unset( $contactmethods['yim'] );
 	unset( $contactmethods['jabber'] );
-	
+
 	return $contactmethods;
 }
 
@@ -124,11 +145,11 @@ function be_contactmethods( $contactmethods ) {
  *
  * @see genesis_inpost_seo_box() Generates the content in the meta box
  */
-function be_add_inpost_seo_box() {
+function dp_add_inpost_seo_box() {
 
 	if ( genesis_detect_seo_plugins() )
 		return;
-		
+
 	foreach ( (array) get_post_types( array( 'public' => true ) ) as $type ) {
 		if ( post_type_supports( $type, 'genesis-seo' ) )
 			add_meta_box( 'genesis_inpost_seo_box', __( 'Theme SEO Settings', 'genesis' ), 'genesis_inpost_seo_box', $type, 'normal', 'default' );
@@ -150,7 +171,7 @@ function be_add_inpost_seo_box() {
  *
  * @return null Returns null if Genesis layouts are not supported
  */
-function be_add_inpost_layout_box() {
+function dp_add_inpost_layout_box() {
 
 	if ( ! current_theme_supports( 'genesis-inpost-layouts' ) )
 		return;
@@ -162,12 +183,12 @@ function be_add_inpost_layout_box() {
 
 }
 
-/** 
+/**
  * Remove Genesis widgets
  *
  * @since 1.0.0
  */
-function be_remove_genesis_widgets() {
+function dp_remove_genesis_widgets() {
     unregister_widget( 'Genesis_eNews_Updates'          );
     unregister_widget( 'Genesis_Featured_Page'          );
     unregister_widget( 'Genesis_Featured_Post'          );
@@ -181,8 +202,11 @@ function be_remove_genesis_widgets() {
  * @since 1.0.0
  * @param string $_genesis_theme_settings_pagehook
  */
-function be_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
+function dp_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
+	//remove_meta_box( 'genesis-theme-settings-version',      $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-style-selector',      $_genesis_theme_settings_pagehook, 'main' );
 	//remove_meta_box( 'genesis-theme-settings-feeds',      $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-layout',      $_genesis_theme_settings_pagehook, 'main' );
 	//remove_meta_box( 'genesis-theme-settings-header',     $_genesis_theme_settings_pagehook, 'main' );
 	remove_meta_box( 'genesis-theme-settings-nav',        $_genesis_theme_settings_pagehook, 'main' );
 	//remove_meta_box( 'genesis-theme-settings-breadcrumb', $_genesis_theme_settings_pagehook, 'main' );
@@ -196,7 +220,7 @@ function be_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
  * Don't Update Theme
  * @since 1.0.0
  *
- * If there is a theme in the repo with the same name, 
+ * If there is a theme in the repo with the same name,
  * this prevents WP from prompting an update.
  *
  * @author Mark Jaquith
@@ -207,7 +231,7 @@ function be_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
  * @return array request arguments
  */
 
-function be_dont_update_theme( $r, $url ) {
+function dp_dont_update_theme( $r, $url ) {
 	if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
 		return $r; // Not a theme update request. Bail immediately.
 	$themes = unserialize( $r['body']['themes'] );
@@ -222,18 +246,18 @@ function be_dont_update_theme( $r, $url ) {
 /**
  * Viewport Meta Tag for Mobile Browsers
  *
- * @author Bill Erickson
- * @link http://www.billerickson.net/code/responsive-meta-tag
+ * @author Del Putnam
  */
-function be_viewport_meta_tag() {
+function dp_viewport_meta_tag() {
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+	//echo '<base target="_parent" />';
 }
 
 /**
- * Footer 
+ * Footer
  *
  */
-function be_footer() {
+function dp_footer() {
 	echo '<div class="one-half first" id="footer-left">' . wpautop( genesis_get_option( 'footer-left', 'child-settings' ) ) . '</div>';
 	echo '<div class="one-half" id="footer-right">' . wpautop( genesis_get_option( 'footer-right', 'child-settings' ) ) . '</div>';
 }
